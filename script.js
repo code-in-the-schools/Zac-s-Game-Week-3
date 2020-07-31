47bf14f767ce0e13d9d3b413fc7b491dd75d7928
Zachiah Zook <zakhazook1@gmail.com>
2020-07-31 22:48:26 +0000

    made paddles move

--- script.js
+++ script.js
@@ -4,33 +4,34 @@ var rectSize = 20;
 var rectangX = canvasW/2, rectangY = canvasH/2;
 var rectangxVel = 1, rectangyVel = 0
 var rectangTop = rectangY + rectSize/2, rectangB = rectangY + rectSize/2, rectangL = rectangX + rectSize/2, rectangR = rectangX + rectSize/2;
-
-    
-
+var paddle;
+var paddleW = 15, paddleH = 60, paddleVel = 5;
+var paddleLeftX = 20, paddleLeftY=canvasH/2;
+var paddleRightX = canvasW-40, paddleRightY = canvasH/2, paddleLTop = paddleLeftY - paddleH/2, 
+paddleLB = paddleLeftY + paddleH/2, 
+paddleRTop = paddleRightY - paddleH/2, 
+paddleRB = paddleRightY + paddleH/2;
 
 function setup() {
 	createCanvas(canvasW, canvasH);
   rectangle = rect(rectangX, rectangY, rectSize, rectSize);
+  rect(20,canvasH/2,paddleW,paddleH);
+  rect(canvasW-40,canvasH/2,paddleW,paddleH);
   rectangxVel = random(-6,6);
 	rectangyVel = random(-6,6);
-  ellipse(mouseX,mouseY,50)
-  rect(50, 50, 100, 100,)
 }
 
 function draw() {
   background(color(0, 221, 255));
 
   bouncingrectangle();
-  
+
+  rect(20,canvasH/2,paddleW,paddleH);
+  rect(canvasW-40,canvasH/2,paddleW,paddleH);
+
   rectangle = rect(rectangX, rectangY, rectSize, rectSize), fill(255);
-  //upper left rectangle
-  rect(50, 50,70, 70), fill(255);
-  //middle rectangle
-  rect(340, 200, 40, 40), fill(255);
-  //bottom rectangle
-  rect(180, 320, 55,55),fill(255);
- 
-  ellipse(mouseX,mouseY,50), fill(0);
+  
+  movepaddles();
   
 }
 
@@ -49,3 +50,24 @@ function draw() {
     rectangyVel = -rectangyVel;
   }
  }
+
+ function movepaddles(){
+paddleLTop = paddleLeftY - paddleH/2, 
+paddleLB = paddleLeftY + paddleH/2, 
+paddleRTop = paddleRightY - paddleH/2, 
+paddleRB = paddleRightY + paddleH/2;
+
+	if (keyIsDown(87) && (paddleLTop > 0)) {
+		paddleLeftY -= paddleVel;
+	} else if (keyIsDown(83) && (paddleLB < canvasH)) {
+		paddleLeftY += paddleVel;
+	}
+
+	if (keyIsDown(79) && (paddleRTop > 0)) {
+		paddleRightY -= paddleVel;
+	} else if (keyIsDown(76) && (paddleRB < canvasH)) {
+		paddleRightY += paddleVel;
+	}
+
+ }
+ 
