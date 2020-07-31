d6a570263bb33708bab184d0f737192340ed7a05
Zachiah Zook <zakhazook1@gmail.com>
2020-07-31 16:24:23 +0000

    added racketts

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var canvasW = 500, canvasH = 400;
 var rectangle;
-var rectSize = 30;
+var rectSize = 20;
 var rectangX = canvasW/2, rectangY = canvasH/2;
 var rectangxVel = 1, rectangyVel = 0
 var rectangTop = rectangY + rectSize/2, rectangB = rectangY + rectSize/2, rectangL = rectangX + rectSize/2, rectangR = rectangX + rectSize/2;
+var rackettW = 15, rackettL = 60;
     
 
 
 function setup() {
 	createCanvas(canvasW, canvasH);
   rectangle = rect(rectangX, rectangY, rectSize, rectSize);
-  myBall_xVel = random(-3,3);
-	myBall_yVel = random(-3,3);
+  rectangxVel = random(-3,6);
+	rectangyVel = random(-3,6);
+  rect(20,200,rackettW,rackettL)
 }
 
 //var [xrot, yrot, t] = [350, 200, 1];
@@ -20,6 +22,9 @@ function draw() {
   background(color(0, 100, 0));
 
   bouncingrectangle();
+
+  rect(20,200,rackettW,rackettL)  
+  rect(canvasW-40,200,rackettW,rackettL) 
 
   rectangle = rect(rectangX, rectangY, rectSize, rectSize);
     	// thing you move
@@ -32,15 +37,16 @@ function draw() {
 }
 
  function bouncingrectangle() {
-  rectangX  = rectangX + rectangxVel;
-	rectangL  = rectangX - rectSize/2;
-	rectangR  = rectangX + rectSize/2;
-	if ( (rectangR > canvasW) || (rectangL < 0) ) {
-		rectangX = -rectangxVel; }
-  
-  rectangY  = rectangY + rectangyVel;
-	rectangTop  = rectangY - rectSize/2;
-	rectangB = rectangY + rectSize/2;
-	if ( (rectangTop > canvasH) || (rectangB < 0) ) {
-		rectangY = -rectangyVel; }
+   rectangX = rectangX + rectangxVel;
+   rectangL = rectangX - rectSize/2;
+   rectangR = rectangX + rectSize/2;
+   if ( ( rectangR > canvasW) || (rectangL < 0) ) {
+     rectangxVel = -rectangxVel;
+   }
+  rectangY = rectangY + rectangyVel;
+  rectangTop = rectangY - rectSize/2;
+  rectangB = rectangY + rectSize/2;
+  if ( (rectangB > canvasH) || (rectangTop < 0) ) {
+    rectangyVel = -rectangyVel;
+  }
  }
