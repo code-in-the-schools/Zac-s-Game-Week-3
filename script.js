83cb3461165d4beb2b674435b5092482e1814fc8
Zachiah Zook <zakhazook1@gmail.com>
2020-08-03 15:33:25 +0000

    completerly changed paddle movement

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ var rectangX = canvasW/2, rectangY = canvasH/2;
 var rectangxVel = 1, rectangyVel = 0
 var rectangTop = rectangY + rectSize/2, rectangB = rectangY + rectSize/2, rectangL = rectangX + rectSize/2, rectangR = rectangX + rectSize/2;
 var paddle;
-var paddleW = 15, paddleH = 60, paddleVel = 5;
-var paddleLeftX = 20, paddleLeftY=canvasH/2;
-var paddleRightX = canvasW-40, paddleRightY = canvasH/2, paddleLTop = paddleLeftY - paddleH/2, 
-paddleLB = paddleLeftY + paddleH/2, 
-paddleRTop = paddleRightY - paddleH/2, 
-paddleRB = paddleRightY + paddleH/2;
+var paddleW = 15, paddleH = 60;
+var [paddleLeftX, paddleLeftY, lxspeed, lyspeed] = [20, canvasH/2, 0, 0]
+var [paddleRightX, paddleRightY, rxspeed, ryspeed] = [canvasW-40, canvasH/2, 0,0]
 
 function setup() {
 	createCanvas(canvasW, canvasH);
   rectangle = rect(rectangX, rectangY, rectSize, rectSize);
-  rect(20,canvasH/2,paddleW,paddleH);
-  rect(canvasW-40,canvasH/2,paddleW,paddleH);
+  rect(paddleLeftX,paddleLeftY,paddleW,paddleH);
+  rect(paddleRightX,paddleRightY,paddleW,paddleH);
   rectangxVel = random(-6,6);
 	rectangyVel = random(-6,6);
 }
@@ -26,16 +23,24 @@ function draw() {
 
   bouncingrectangle();
 
-  rect(20,canvasH/2,paddleW,paddleH);
-  rect(canvasW-40,canvasH/2,paddleW,paddleH);
+  rect(paddleLeftX,paddleLeftY,paddleW,paddleH);
+  rect(paddleRightX,paddleRightY,paddleW,paddleH);
 
   rectangle = rect(rectangX, rectangY, rectSize, rectSize), fill(255);
-  
-  movepaddles();
-  
+
+  if(paddleLeftX >= 0 && paddleLeftX + 50 <= 500) paddleLeftX += lxspeed;
+	if(paddleLeftX >= 0 && paddleLeftX + 50 <= 500) paddleLeftX += lyspeed;
+
+  if( paddleRightX >= 0 && paddleRightX + 50 <= 500) paddleRightX += rxspeed;
+	if(paddleRightX >= 0 && paddleRightX + 50 <= 500) paddleRightX += ryspeed;
+
+  keyPressed();
+  keyReleased();
+  keyPressed();
+  keyReleased();
 }
 
- function bouncingrectangle() {
+function bouncingrectangle() {
    fill(207,181,59),
    rectangX = rectangX + rectangxVel;
    rectangL = rectangX - rectSize/2;
@@ -49,25 +54,90 @@ function draw() {
   if ( (rectangB > canvasH) || (rectangTop < 0) ) {
     rectangyVel = -rectangyVel;
   }
- }
+}
 
- function movepaddles(){
-paddleLTop = paddleLeftY - paddleH/2, 
-paddleLB = paddleLeftY + paddleH/2, 
-paddleRTop = paddleRightY - paddleH/2, 
-paddleRB = paddleRightY + paddleH/2;
-
-	if (keyIsDown(87) && (paddleLTop > 0)) {
-		paddleLeftY -= paddleVel;
-	} else if (keyIsDown(83) && (paddleLB < canvasH)) {
-		paddleLeftY += paddleVel;
+//leftPaddle
+function keyPressed() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			lxspeed = -2;
+			break;
+		case 39:
+		case 68:
+			lxspeed = 2;
+			break;
+		case 38:
+		case 87:
+			lyspeed = -2;
+			break;
+		case 40:
+		case 83:
+			lyspeed = 2;
+			break;
 	}
-
-	if (keyIsDown(79) && (paddleRTop > 0)) {
-		paddleRightY -= paddleVel;
-	} else if (keyIsDown(76) && (paddleRB < canvasH)) {
-		paddleRightY += paddleVel;
+}
+//leftPaddle
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			lxspeed = 0;
+			break;
+		case 39:
+		case 68:
+			lxspeed = 0;
+			break;
+		case 38:
+		case 87:
+			lyspeed = 0;
+			break;
+		case 40:
+		case 83:
+			lyspeed = 0;
+			break;
 	}
-
- }
+}
+//rightPaddle
+function keyPressed() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			rxspeed = -2;
+			break;
+		case 39:
+		case 68:
+			rxspeed = 2;
+			break;
+		case 38:
+		case 87:
+			ryspeed = -2;
+			break;
+		case 40:
+		case 83:
+			ryspeed = 2;
+			break;
+	}
+}
+//rightPaddle
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			rxspeed = 0;
+			break;
+		case 39:
+		case 68:
+			rxspeed = 0;
+			break;
+		case 38:
+		case 87:
+			ryspeed = 0;
+			break;
+		case 40:
+		case 83:
+			ryspeed = 0;
+			break;
+	}
+}
  
